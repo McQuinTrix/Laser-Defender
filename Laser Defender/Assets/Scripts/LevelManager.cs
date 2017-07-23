666f190073c1a091c7af3e7e22c890994aa2f6eb
Harshal Carpenter <harshal.carpenter@gmail.com>
2017-07-23 03:04:01 -0700

    Laser Defender First Push

---
+++ Laser Defender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	//Whenver you want to let a function to be accessed by Unity, make it "public"
+	public void LoadLevel(string name){
+		Debug.Log ("Level Load requested for "+name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit this game ?");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void Correct(){
+		Debug.Log ("Computer Won!");
+		LoadLevel ("Lose");
+	}
+}
---
+++ Laser Defender/Assets/Scripts/SpaceShipController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipController : MonoBehaviour {
+
+	private GameObject spaceShip;
+	private float speed = 7f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)){
+			moveSpaceShip ();
+		}
+	}
+
+	void moveSpaceShip(){
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += new Vector3 (speed * Time.deltaTime,0,0);
+		}else if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += new Vector3 (-speed* Time.deltaTime,0,0);
+		}
+	}
+}
